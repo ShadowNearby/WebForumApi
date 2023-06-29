6edfd27a60fb78f969de371124902532d5c4b90f
shadow <yan.jingsheng@outlook.com>
2023-06-29 16:59:23 +0800

    🎨rename to DataPath

--- src/WebForumApi.Application/Features/Files/FileUpload/FileUploadHandler.cs
+++ src/WebForumApi.Application/Features/Files/FileUpload/FileUploadHandler.cs
@@ -11,18 +11,17 @@ namespace WebForumApi.Application.Features.Files.FileUpload;
 
 public class FileUploadHandler : IRequestHandler<FileUploadRequest, Result<FileDto>>
 {
-    private static readonly string _dataPath = Directory.GetParent(Environment.CurrentDirectory)!.Parent!.FullName;
+    private static readonly string DataPath = Directory.GetParent(Environment.CurrentDirectory)!.Parent!.FullName;
     public async Task<Result<FileDto>> Handle(FileUploadRequest request, CancellationToken cancellationToken)
     {
         IFormFile file = request.File;
         string filename = $"{Guid.NewGuid().ToString()}{file.FileName}";
-        string filepath = $"{_dataPath}/wwwroot/images/{filename}";
+        string filepath = $"{DataPath}/wwwroot/images/{filename}";
         await using FileStream stream = File.Create(filepath);
         await file.CopyToAsync(stream, cancellationToken);
         return new Result<FileDto>(new FileDto
         {
             Url = $"http://localhost:5000/images/{filename}"
         });
-        return Result.Success();
     }
 }
