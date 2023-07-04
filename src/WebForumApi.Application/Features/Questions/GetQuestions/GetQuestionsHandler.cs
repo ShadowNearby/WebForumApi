5ed41ca5f96261edb04266d9c4a1e043ac340f5c
shadow <yan.jingsheng@outlook.com>
2023-07-04 10:17:51 +0800

    ✨add cache

--- src/WebForumApi.Application/Features/Questions/GetQuestions/GetQuestionsHandler.cs
+++ src/WebForumApi.Application/Features/Questions/GetQuestions/GetQuestionsHandler.cs
@@ -1,12 +1,15 @@
 using Mapster;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using WebForumApi.Application.Common;
 using WebForumApi.Application.Common.Responses;
 using WebForumApi.Application.Extensions;
+using WebForumApi.Application.Extensions.Cache;
 using WebForumApi.Application.Features.Questions.Dto;
 using WebForumApi.Domain.Entities;
 
@@ -14,11 +17,13 @@ namespace WebForumApi.Application.Features.Questions.GetQuestions;
 
 public class GetQuestionsHandler : IRequestHandler<GetQuestionsRequest, PaginatedList<QuestionCardDto>>
 {
+    private readonly ICacheService _cache;
     private readonly IContext _context;
 
-    public GetQuestionsHandler(IContext context)
+    public GetQuestionsHandler(IContext context, ICacheService cache)
     {
         _context = context;
+        _cache = cache;
     }
 
     public async Task<PaginatedList<QuestionCardDto>> Handle(GetQuestionsRequest request,
@@ -35,10 +40,24 @@ public class GetQuestionsHandler : IRequestHandler<GetQuestionsRequest, Paginate
                         !string.IsNullOrEmpty(request.KeyWord),
                         x => EF.Functions.Like(x.Title, $"%{request.KeyWord}%")
                     );
-                return await newestQuestions
+                PaginatedList<QuestionCardDto> result = await newestQuestions
                     .ProjectToType<QuestionCardDto>()
                     .ToPaginatedListAsync(request.CurrentPage, request.PageSize);
+                if (string.IsNullOrEmpty(request.KeyWord) && request.CurrentPage == 1)
+                {
+                    PaginatedList<QuestionCardDto>? cacheResult = await _cache.GetAsync<PaginatedList<QuestionCardDto>>("question_newest", cancellationToken);
+                    if (cacheResult != null)
+                    {
+                        return cacheResult;
+                    }
+
+                    await _cache.SetAsync("question_newest", result, TimeSpan.FromDays(1), cancellationToken);
+                }
+
+                LoggerMessage.Define(LogLevel.Information, new EventId(0), request.PageSize.ToString());
+                return result;
             case "heat":
+            case "":
                 // order by answer number
                 IQueryable<Question> heatQuestions = _context.Questions
                     .OrderByDescending(x => x.AnswerCount)
@@ -51,7 +70,7 @@ public class GetQuestionsHandler : IRequestHandler<GetQuestionsRequest, Paginate
                     .ToPaginatedListAsync(request.CurrentPage, request.PageSize);
             default:
                 // select unanswered questions
-                IQueryable<Question>? unansweredQuestions = _context.Questions
+                IQueryable<Question> unansweredQuestions = _context.Questions
                         .Where(x => x.Answers.Count == 0)
                         .OrderByDescending(x => x.CreateTime)
                         .WhereIf(
--- src/WebForumApi.Application/Features/Questions/GetQuestions/GetQuestionsRequest.cs
+++ src/WebForumApi.Application/Features/Questions/GetQuestions/GetQuestionsRequest.cs
@@ -2,14 +2,12 @@ using MediatR;
 using WebForumApi.Application.Common.Requests;
 using WebForumApi.Application.Common.Responses;
 using WebForumApi.Application.Features.Questions.Dto;
-using WebForumApi.Application.Features.Users.Dto;
-using WebForumApi.Application.Features.Users.GetUsers;
 
 namespace WebForumApi.Application.Features.Questions.GetQuestions;
 
 public record GetQuestionsRequest : PaginatedRequest, IRequest<PaginatedList<QuestionCardDto>>
 {
-    public string Tab { get; init; } = null!;
+    public string Tab { get; init; } = "";
 
     // tab in ["newest", "heat", "unanswered"]
     public string? KeyWord { get; init; }
